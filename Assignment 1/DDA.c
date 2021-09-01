a08998c61d5eeedfa22a4dec56b426f3acb1038f
Gaganpreet Khurana <giftykhurana0@gmail.com>
2021-09-01 16:38:30 +0530

    Final Testing Left

--- Assignment 1/DDA.c
+++ Assignment 1/DDA.c
@@ -1,87 +1,112 @@
+/*
+    C program to implement DDA line drawing algorithm using OpenGL
+*/
 #include <stdio.h>
-#include<GL/glut.h>
-#include<math.h>
+#include <GL\glut.h>
+#include <math.h>
+
+// Coordinates
 int x1;
 int y_1;
 int x2;
 int y2;
-void setPixel( int x,int y){
+
+// function to set pixel
+void setPixel(int x, int y)
+{
     glBegin(GL_POINTS);
-    glVertex2i(x,y);
+    glVertex2i(x, y);
     glEnd();
     glFlush();
 }
-void DDA()
- {
-     int dx = abs(x1 - x2);
-     int dy = abs(y_1 - y2);
-     float steps = fmax(dx, dy);
-     float xINC,yINC;
-     xINC = dx / steps;
-     yINC = dy / steps;
-     float X = (float)x1;
-     float Y = (float)y_1;
 
-     for (int i = 0; i <= steps; i++)
-     {
-         printf("Step ");
-         printf("%d", i + 1  );
-         printf( ": " );
-         printf( "%f",X );
-         printf(" " );
-         printf("%f", Y);
-         printf( "\n");
-         setPixel((int)round(X), (int)round(Y));
-         X += xINC;
-         Y += yINC;
-     }
+//DDA algorithm
+void DDA()
+{
+    // Abs difference
+    int dx = abs(x1 - x2);
+    int dy = abs(y_1 - y2);
+
+    // Steps
+    float steps = fmax(dx, dy);
+
+    //Calculate Increments
+    float xINC, yINC;
+    xINC = dx / steps;
+    yINC = dy / steps;
+
+    // Initial Points
+    float X = (float)x1;
+    float Y = (float)y_1;
+
+    printf("Starting point: (%d,%d)\n", x1, y_1);
+    printf("End point: (%d,%d)\n", x2, y2);
+    printf("Absolute differences: (%d,%d)\n", dx, dy);
+    printf("Steps: %f\n", steps);
+    printf("Increments: (%f,%f")\n,xINC,yINC);
+    printf("STEP:    |(X,Y)|      Point")
+
+        for (int i = 0; i <= steps; i++)
+    {
+        printf("%d:    |(%f,%f)|      (%d,%d)\n", i + 1, X, Y, (int)round(X), (int)round(Y));
+
+        //Draw
+        setPixel((int)round(X), (int)round(Y));
+
+        //Increment
+        X += xINC;
+        Y += yINC;
+    }
 }
+
 // function to initialize
-void myInit (void)
+void myInit(void)
 {
-	// making background color black as first
-	// 3 arguments all are 0.0
-	glClearColor(0.0, 0.0, 0.0, 1.0);
-	
-	// making picture color green (in RGB mode), as middle argument is 1.0
-	glColor3f(1.0, 1.0, 0.0);
-	
-	// breadth of picture boundary is 1 pixel
-	glPointSize(3.0);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	
-	// setting window dimension in X- and Y- direction
-	gluOrtho2D(-780, 780, -420, 420);
+    // making background color black as first
+    // 3 arguments all are 0.0
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+
+    // making picture color green (in RGB mode), as middle argument is 1.0
+    glColor3f(0.0, 1.0, 0.0);
+
+    // breadth of picture boundary is 3 pixel
+    glPointSize(3.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    // setting window dimension in X- and Y- direction
+    gluOrtho2D(-780, 780, -420, 420);
 }
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-    
+    // Take coordinates as input from user
     printf("Enter coordinates of point A:\n");
     printf("x: ");
-    scanf("%d",&x1);
-    printf("\ny: ");
-    scanf("%d",&y_1);
-    printf("\nEnter coordinates of point B:\n");
+    scanf("%d", &x1);
+    printf("y: ");
+    scanf("%d", &y_1);
+    printf("Enter coordinates of point B:\n");
     printf("x: ");
-    scanf("%d",&x2);
-    printf("\ny: ");
-    scanf("%d",&y2);
+    scanf("%d", &x2);
+    printf("y: ");
+    scanf("%d", &y2);
     printf("\n");
 
     glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	
-	// giving window size in X- and Y- direction
-	glutInitWindowSize(1024, 720);
-	glutInitWindowPosition(0, 0);
-	
-	// Giving name to window
-	glutCreateWindow("DDA");
-	myInit();
-	
-	glutDisplayFunc(DDA);
-	glutMainLoop();
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+
+    // giving window size in X- and Y- direction
+    glutInitWindowSize(1024, 720);
+    glutInitWindowPosition(0, 0);
+
+    // Giving name to window
+    glutCreateWindow("DDA");
+    myInit();
+
+    glutDisplayFunc(DDA);
+    glutMainLoop();
     return 0;
 }
-//g++ DDA.c -lGL -lGLU -lglut
+
+//Terminal command to run on Linux
+//gcc DDA.c -lGL -lGLU -lglut && ./a.out
