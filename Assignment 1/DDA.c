5b17a17bfb10b0f54e9f76e0b96f693c670fb410
GaganpreetKhurana <giftykhurana0@gmail.com>
2021-09-03 00:13:13 +0530

    Bug Fixes

--- Assignment 1/DDA.c
+++ Assignment 1/DDA.c
@@ -3,6 +3,12 @@
     
     Keep large absolute difference to see the animation so that the
     number of steps are > 150
+
+    ORIGIN (0,0) is set at TOP left corner - x:[0,1920], y:[0,1080]
+    To move it to center of screen comment line 94 and uncomment line 97 - x:[-960,960], y:[-540,540]
+
+    Terminal command to run on Linux
+    g++ DDA.c -lGL -lGLU -lglut && ./a.out
 */
 #include <stdio.h>
 #include <GL/glut.h>
@@ -13,6 +19,13 @@ int x1;
 int y_1;
 int x2;
 int y2;
+
+//SET SCREEN SIZE/ORIGIN
+const int SCREEN_WIDTH_DDA = 1920;
+const int SCREEN_HEIGHT_DDA = 1080;
+const int SCREEN_ORIGIN_X_DDA = 0;
+const int SCREEN_ORIGIN_Y_DDA = 0;
+
 
 // function to set pixel
 void setPixel(int x, int y)
@@ -27,11 +40,11 @@ void setPixel(int x, int y)
 void DDA()
 {
     // Abs difference
-    int dx = abs(x1 - x2);
-    int dy = abs(y_1 - y2);
-
+    int dx =  x2 - x1; 
+    int dy = y2 - y_1;
+    
     // Steps
-    float steps = fmax(dx, dy);
+    float steps = fmax(abs(dx),abs(dy));
 
     //Calculate Increments
     float xINC, yINC;
@@ -60,6 +73,7 @@ void DDA()
         X += xINC;
         Y += yINC;
     }
+    
 }
 
 // function to initialize
@@ -78,7 +92,12 @@ void myInit(void)
     glLoadIdentity();
 
     // setting window dimension in X- and Y- direction
-    gluOrtho2D(-1024, 1024, -720, 720);
+
+    // ORIGIN at TOP LEFT
+    gluOrtho2D(0, SCREEN_WIDTH_DDA, SCREEN_HEIGHT_DDA, 0);
+
+    // ORIGIN at CENTER
+    //gluOrtho2D(-(SCREEN_WIDTH_DDA/2),(SCREEN_WIDTH_DDA/2),-(SCREEN_HEIGHT_DDA/2),(SCREEN_HEIGHT_DDA/2));
 }
 int main(int argc, char **argv)
 {
@@ -99,17 +118,16 @@ int main(int argc, char **argv)
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 
     // giving window size in X- and Y- direction
-    glutInitWindowSize(1024, 720);
-    glutInitWindowPosition(0, 0);
+    glutInitWindowSize(SCREEN_WIDTH_DDA, SCREEN_HEIGHT_DDA);
+    glutInitWindowPosition(SCREEN_ORIGIN_X_DDA, SCREEN_ORIGIN_Y_DDA);
 
     // Giving name to window
     glutCreateWindow("DDA");
     myInit();
 
     glutDisplayFunc(DDA);
+    
     glutMainLoop();
+    
     return 0;
 }
-
-//Terminal command to run on Linux
-//g++ DDA.c -lGL -lGLU -lglut && ./a.out
