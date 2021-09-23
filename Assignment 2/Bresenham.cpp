96438bdf8b1d8df123b8c9b197ed2adc05264fb5
Gaganpreet Khurana <giftykhurana0@gmail.com>
2021-09-23 14:59:38 +0530

    Done for m<=1

--- Assignment 2/Bresenham.cpp
+++ Assignment 2/Bresenham.cpp
@@ -2,7 +2,7 @@
     Assumptions:
     1. Top Left is Origin(0,0)
     2. x1 < x2 and y1< y2
-    3. Slope of the line is between 0 and 1
+    3. Slope of the line is between 0 and 1.For other slopes we need to modify the algorithm slightly
 
     Command to run (Windows):
     g++ Bresenham.cpp -lbgi -lgdi32 -lcomdlg32 -luuid -loleaut32 -lole32
@@ -12,20 +12,20 @@
 using namespace std;
 
 //Bresenham algorithm
-void Bresenham(int x1, int y_1, int x2, int y2)
+void Bresenham(int x1, int y1, int x2, int y2)
 {
     // Difference
     int dx = x2 - x1;
-    int dy = y2 - y_1;
+    int dy = y2 - y1;
 
     // Starting Points
     int X = x1;
-    int Y = y_1;
+    int Y = y1;
 
     // Decision Variable
     int p = (2 * dy) - dx;
 
-    cout << "Starting point: (" + to_string(x1) + "," + to_string(y_1) + ")\n";
+    cout << "Starting point: (" + to_string(x1) + "," + to_string(y1) + ")\n";
     cout << "End Point: (" + to_string(x2) + "," + to_string(y2) + ")\n";
     cout << "Difference: (" + to_string(dx) + "," + to_string(dy) + ")\n";
     cout << "Decision Variable: " + to_string(p) + "\n";
@@ -38,8 +38,8 @@ void Bresenham(int x1, int y_1, int x2, int y2)
     outtextxy(200, 5, buffer);
 
     //Start Point
-    sprintf(buffer, "(%d,%d)", x1, y_1);
-    outtextxy(x1 - 5, y_1 - 5, buffer);
+    sprintf(buffer, "(%d,%d)", x1, y1);
+    outtextxy(x1 - 5, y1 - 5, buffer);
 
     //End Point
     sprintf(buffer, "(%d,%d)", x2, y2);
@@ -62,7 +62,6 @@ void Bresenham(int x1, int y_1, int x2, int y2)
         X++;
     }
 
-    // cout << "(" << X << "," << Y << ")       |   " << p << endl;
 }
 
 int main()
@@ -72,27 +71,25 @@ int main()
     initgraph(&gDrive, &gMode, NULL);
 
     // Coordinates
-    int x1 = 0;
-    int y_1 = 0;
-    int x2 = 183;
-    int y2 = 146;
+    int x1;
+    int y1;
+    int x2;
+    int y2;
 
     // Take coordinates as input from user
     cout << "Enter coordinates of point A:\n";
-    // cout<<"x: ";
-    // cin>>x1;
-    // cout<<"y: ";
-    // cin>>y_1;
-    // cout << "Enter coordinates of point B:\n";
-    // cout << "x: ";
-    // cin >> x2;
-    // cout << "y: ";
-    // cin >> y2;
-    // cout << "\n";
+    cout << "x: ";
+    cin >> x1;
+    cout << "y: ";
+    cin >> y1;
+    cout << "Enter coordinates of point B:\n";
+    cout << "x: ";
+    cin >> x2;
+    cout << "y: ";
+    cin >> y2;
+    cout << "\n";
 
-    Bresenham(x1, y_1, x2, y2);
+    Bresenham(x1, y1, x2, y2);
     getch();
-    // closegraph();
+    closegraph();
 }
-
-//g++ Bresenham.cpp -lbgi -lgdi32 -lcomdlg32 -luuid -loleaut32 -lole32
--- Assignment 2/MidPoint.cpp
+++ Assignment 2/MidPoint.cpp
@@ -12,24 +12,20 @@
 using namespace std;
 
 //MidPoint algorithm
-void MidPoint(int x1, int y_1, int x2, int y2)
+void MidPoint(int x1, int y1, int x2, int y2)
 {
     // Difference
     int dx = x2 - x1;
-    int dy = y2 - y_1;
+    int dy = y2 - y1;
 
     // Starting Points
     int X = x1;
-    int Y = y_1;
+    int Y = y1;
 
-    // Decision Variable
-    int p = (2 * dy) - dx;
-
-    cout << "Starting point: (" + to_string(x1) + "," + to_string(y_1) + ")\n";
+    cout << "Starting point: (" + to_string(x1) + "," + to_string(y1) + ")\n";
     cout << "End Point: (" + to_string(x2) + "," + to_string(y2) + ")\n";
     cout << "Difference: (" + to_string(dx) + "," + to_string(dy) + ")\n";
-    cout << "Decision Variable: " + to_string(p) + "\n";
-    cout << "Points(X,Y)     |    Decision Variable\n";
+    cout << "Points(X,Y)     |    Decision Parameter\n";
 
     // for printing text at desired screen location.
     char buffer[30];
@@ -38,31 +34,53 @@ void MidPoint(int x1, int y_1, int x2, int y2)
     outtextxy(200, 5, buffer);
 
     //Start Point
-    sprintf(buffer, "(%d,%d)", x1, y_1);
-    outtextxy(x1 - 5, y_1 - 5, buffer);
+    sprintf(buffer, "(%d,%d)", x1, y1);
+    outtextxy(x1 - 5, y1 - 5, buffer);
 
     //End Point
     sprintf(buffer, "(%d,%d)", x2, y2);
     outtextxy(x2 + 5, y2 + 5, buffer);
 
-    while (X <= x2)
+    if (dy <= dx)
     {
-        cout << "(" << X << "," << Y << ")       |   " << p << endl;
-        delay(50);
-        putpixel(X, Y, 7);
-        if (p >= 0)
+        int d = dy - (dx / 2);
+        while (X <= x2)
         {
-            Y++;
-            p += (2 * dy) - (2 * dx);
+            cout << "(" << X << "," << Y << ")       |   " << d << endl;
+            delay(50);
+            putpixel(X, Y, 7);
+            if (d >= 0)
+            {
+                Y++;
+                d += dy - dx;
+            }
+            else
+            {
+                d += dy;
+            }
+            X++;
         }
-        else
-        {
-            p += 2 * dy;
-        }
-        X++;
     }
-
-    // cout << "(" << X << "," << Y << ")       |   " << p << endl;
+    else
+    {
+        int d = dy - (dx / 2);
+        while (Y <= y2)
+        {
+            cout << "(" << X << "," << Y << ")       |   " << d << endl;
+            delay(50);
+            putpixel(X, Y, 7);
+            if (d >= 0)
+            {
+                X++;
+                d += dx - dy;
+            }
+            else
+            {
+                d += dx;
+            }
+            Y++;
+        }
+    }
 }
 
 int main()
@@ -73,26 +91,24 @@ int main()
 
     // Coordinates
     int x1 = 0;
-    int y_1 = 0;
+    int y1 = 0;
     int x2 = 183;
     int y2 = 146;
 
     // Take coordinates as input from user
     cout << "Enter coordinates of point A:\n";
-    // cout<<"x: ";
-    // cin>>x1;
-    // cout<<"y: ";
-    // cin>>y_1;
-    // cout << "Enter coordinates of point B:\n";
-    // cout << "x: ";
-    // cin >> x2;
-    // cout << "y: ";
-    // cin >> y2;
-    // cout << "\n";
+    cout << "x: ";
+    cin >> x1;
+    cout << "y: ";
+    cin >> y1;
+    cout << "Enter coordinates of point B:\n";
+    cout << "x: ";
+    cin >> x2;
+    cout << "y: ";
+    cin >> y2;
+    cout << "\n";
 
-    MidPoint(x1, y_1, x2, y2);
+    MidPoint(x1, y1, x2, y2);
     getch();
-    // closegraph();
+    closegraph();
 }
-
-//g++ MidPoint.cpp -lbgi -lgdi32 -lcomdlg32 -luuid -loleaut32 -lole32
