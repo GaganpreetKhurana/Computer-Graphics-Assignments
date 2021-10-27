929cf2573ae58d176e5923d1dfac5d7f07118e2f
Gaganpreet Khurana <giftykhurana0@gmail.com>
2021-10-27 14:39:33 +0530

    Flood Fill done

--- Assignment 5/FloodFill.cpp
+++ Assignment 5/FloodFill.cpp
@@ -9,21 +9,22 @@
 
 #include <graphics.h>
 #include <iostream>
-#include <math.h>
 using namespace std;
-void flood_fill_colour(int x, int y, int fill_colour, int boundary_colour)
+
+const int BACKGROUND_COLOUR = 0; // Black
+void flood_fill_colour(int x, int y, int fill_colour)
 {
     // Check if current pixel colour is not equal to boundary/colour to be filled
-    if (getpixel(x, y) != boundary_colour && getpixel(x, y) != fill_colour)
+    if (getpixel(x, y) == BACKGROUND_COLOUR && getpixel(x, y) != fill_colour)
     {
         // Fill colour
         putpixel(x, y, fill_colour);
 
         // Traverse 4 adjacent pixels
-        flood_fill_colour(x + 1, y, fill_colour, boundary_colour);
-        flood_fill_colour(x, y + 1, fill_colour, boundary_colour);
-        flood_fill_colour(x - 1, y, fill_colour, boundary_colour);
-        flood_fill_colour(x, y - 1, fill_colour, boundary_colour);
+        flood_fill_colour(x + 1, y, fill_colour);
+        flood_fill_colour(x, y + 1, fill_colour);
+        flood_fill_colour(x - 1, y, fill_colour);
+        flood_fill_colour(x, y - 1, fill_colour);
     }
 }
 int main()
@@ -38,40 +39,38 @@ int main()
 
     int coordinates[2 * (numberOfSides + 1)];
 
-    // To take seed coordinate as average
-    int sum_x = 0, sum_y = 0;
-
     // Take coordinates as input from user
     cout << "Enter Vertices (In Cyclic order):    x   y\n";
     for (int i = 0; i < numberOfSides; i++)
     {
         cout << "Vertice " << i + 1 << ": ";
         cin >> coordinates[2 * i] >> coordinates[2 * i + 1];
-        sum_x += coordinates[2 * i];
-        sum_y += coordinates[2 * i + 1];
     }
     coordinates[2 * numberOfSides] = coordinates[0];
     coordinates[2 * numberOfSides + 1] = coordinates[1];
 
-    // SET seed and fill color
+    // Take seed as input and set fill color
+
     int x_seed, y_seed, fill_colour;
     fill_colour = 14; // YELLOW
-    // Seed is the average of the coordinates
-    x_seed = sum_x / numberOfSides;
-    y_seed = sum_y / numberOfSides;
+    cout << "Enter seed coordinates:\nx: ";
+    cin >> x_seed;
+    cout << "y: ";
+    cin >> y_seed;
 
     initgraph(&gDrive, &gMode, NULL);
 
-    // Set colour to RED
+    // Set boundary colour to RED
     int boundary_colour = 4;
     setcolor(boundary_colour);
 
     // Draw Polygon
     drawpoly(numberOfSides + 1, coordinates);
-    
+
     //Fill Colour using Boundary Fill
-    flood_fill_colour(x_seed, y_seed, fill_colour, boundary_colour);
+    flood_fill_colour(x_seed, y_seed, fill_colour);
     getch();
     closegraph();
-    
+
+    cout << "Complete\n";
 }
